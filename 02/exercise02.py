145e39bc666b46f8c8485ac8320bbce81cefd3f8
Jessica <jessicasuarezc@outlook.com>
2025-12-10 00:25:45 +0200

    exercise 03

--- 02/exercise02.py
+++ 02/exercise02.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 
 db_name="question2.db"
-#create db - remember to leave conn and cursor duplicate so code can run!!
+
 conn = sqlite3.connect(db_name)
 cursor=conn.cursor()
 
