9f488af2763932d6cfe831dc24e4b8b63c10a4a8
Jessica <jessicasuarezc@outlook.com>
2025-12-31 00:46:17 +0200

    23.2 DONE

--- 21-30/23/exercise23.py
+++ 21-30/23/exercise23.py
@@ -31,14 +31,10 @@ cursor=conn.cursor()
 # conn.commit()
 
 first_method="SELECT * FROM (SELECT * ,LAG(is_empty,1) OVER(ORDER BY seat_no) AS prev_1, LAG(is_empty,2) OVER(ORDER BY seat_no) AS prev_2, LEAD(is_empty,1) OVER(ORDER BY seat_no) AS next_1, LEAD(is_empty,2) OVER (ORDER BY seat_no) AS next_2 FROM bms) A WHERE is_empty='Y' AND prev_1='Y' and prev_2='Y' OR(is_empty='Y' and prev_1='Y' and next_1='Y') OR (is_empty='Y' and next_1='Y' and next_2='Y')"
-df=pd.read_sql(first_method,conn)
+second_method="SELECT * FROM (SELECT *, SUM(CASE WHEN is_empty = 'Y' THEN 1 ELSE 0 END) OVER (ORDER BY seat_no ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS prev_2, SUM(CASE WHEN is_empty = 'Y' THEN 1 ELSE 0 END) OVER (ORDER BY seat_no ROWS BETWEEN 1 PRECEDING AND 1 FOLLOWING) AS prev_next_1, SUM(CASE WHEN is_empty = 'Y' THEN 1 ELSE 0 END) OVER (ORDER BY seat_no ROWS BETWEEN CURRENT ROW AND 2 FOLLOWING) AS next_2 FROM BMS) A WHERE prev_2 = 3 OR prev_next_1 = 3 OR next_2 = 3"
+df=pd.read_sql(second_method,conn)
 print(df)
 conn.close()
-
-# second_method="SELECT * FROM bms"
-# df=pd.read_sql(second_method,conn)
-# print(df)
-# conn.close()
 
 # third_method="SELECT * FROM bms"
 # df=pd.read_sql(third_method,conn)
