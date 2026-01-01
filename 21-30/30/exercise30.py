11cc3c1eda549327ef754e1c1d363223f0ead1e9
Jessica <jessicasuarezc@outlook.com>
2026-01-01 20:21:51 +0200

    initial data

---
+++ 21-30/30/exercise30.py
@@ -0,0 +1,40 @@
+import os
+import sqlite3
+import pandas as pd
+
+db_name = "exercise30.db"
+conn = sqlite3.connect(db_name)
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS students(
+    studentid INT,
+    studentname TEXT,
+    subject TEXT,
+    marks INT,
+    testid INT,
+    testdate DATE
+)""")
+conn.commit()
+
+students_data = [
+    (2, 'Max Ruin', 'Subject1', 63, 1, '2022-01-02'),
+    (3, 'Arnold', 'Subject1', 95, 1, '2022-01-02'),
+    (4, 'Krish Star', 'Subject1', 61, 1, '2022-01-02'),
+    (5, 'John Mike', 'Subject1', 91, 1, '2022-01-02'),
+    (4, 'Krish Star', 'Subject2', 71, 1, '2022-01-02'),
+    (3, 'Arnold', 'Subject2', 32, 1, '2022-01-02'),
+    (5, 'John Mike', 'Subject2', 61, 2, '2022-11-02'),
+    (1, 'John Deo', 'Subject2', 60, 1, '2022-01-02'),
+    (2, 'Max Ruin', 'Subject2', 84, 1, '2022-01-02'),
+    (2, 'Max Ruin', 'Subject3', 29, 3, '2022-01-03'),
+    (5, 'John Mike', 'Subject3', 98, 2, '2022-11-02')
+]
+
+cursor.executemany("INSERT INTO students VALUES(?,?,?,?,?,?)", students_data)
+conn.commit()
+
+querytoexec = "SELECT * FROM students"
+df = pd.read_sql(querytoexec, conn)
+print(df)
+conn.close()
